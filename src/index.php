e13ee533ac7b5c80905e72eb5e151e04c2dd30e7
Emre C <emre@devinternet.com>
2014-06-18 16:18:08 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -6,24 +6,18 @@ class Foo
 	private $i = 5; // Unused 
 	private $j = 6; 
 	
-	
 	public function addOne() 
 	{ 
 		return $this->j++; 
-	} 
-
+	}
 
 	public function doSomething() 
 	{ 
 		$i = 5; // Unused 
 	}
 
-
 	private function bar($howdy) 
 	{ 
 	 	// $howdy is not used 
 	}
-
 }
-
-
