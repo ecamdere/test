5e204af59b876e5127f7ddc214ccceebcf20989a
Emre C <emre@devinternet.com>
2014-06-18 16:16:20 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,10 +1,12 @@
 <?php
 
 class Foo  
-{ 
+{
 	private static $FOO = 2; // Unused 
 	private $i = 5; // Unused 
 	private $j = 6; 
+	
+	
 	public function addOne() 
 	{ 
 		return $this->j++; 
@@ -16,10 +18,12 @@ class Foo
 		$i = 5; // Unused 
 	}
 
+
 	private function bar($howdy) 
 	{ 
 	 	// $howdy is not used 
 	}
+
 }
 
 
