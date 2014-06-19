040363e51f06ed27d3c31b258c72e028d94869a8
Emre CAMDERE <emre@devinternet.com>
2014-06-19 17:07:41 +0300

    blaa blaaa

--- src/projectName.php
+++ src/projectName.php
@@ -6,24 +6,27 @@ echo PHP_EOL. __FILE__;
 class FOO
 {
 
-    private static $FOO = 2; // Unused 
-	private $i = 5; // Unused 
+    private static $FOO = 2; // Unused
+	private $i = 5; // Unused
 	private $amount;
 
-	
-	public function addOne() 
-	{ 
-		return $this->amount++; 
+
+	public function addOne()
+	{
+		return $this->amount++;
 	}
 
-	public function doSomething() 
-	{ 
-		$i = 5; // Unused 
+	public function doSomething()
+	{
+		$i = 5; // Unused
 	}
 
-	private function bar($howdy) 
-	{ 
-	 	// $howdy is not used 
+	private function bar($howdy)
+	{
+	 	// $howdy is not used
 	}
 
 }
+
+echo 'errorrrrrrrr
+
